3c2b6f304a59684a09b34a4576546caa0c382770
ilyasmahfud <ilyasmahfud39@gmail.com>
2021-11-10 21:50:30 +0800

    sukses get layout.blade.html

---
+++ resources/views/cars/create.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('mainContent')
+    <h1>Create Car</h1>
+    <form action="create" method="post" class="form-horizontal">
+        <fieldset>
+            <legend></legend>
+
+            <div class="form-group">
+                <label for="" class="col-md-4"></label>
+            </div>
+        </fieldset>
+    </form>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Learn Laravel</title>
+</head>
+<body>
+    <div class="container well card">
+        <h1 class="text-center" style="margin-top: 5px">
+            CRUD operation on MySQL using Laravel
+        </h1>
+        <h3 class="text-center" style="margin-top: 5px">
+            create layout template file
+        </h3>
+    </div>
+
+    <div class="container">
+        @yield('mainContent')
+    </div>
+</body>
+</html>
